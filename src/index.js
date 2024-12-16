73c15e115005e20792e8dc9bc41d9c988cab8e98
Jade S <thedavidsoto@gmail.com>
2024-12-16 11:48:50 +0800

    Finalize final look for To-Do List UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./style.css";
 
 console.log("Webpack is working!");
+
+
+// Things to do:
+
+// (NOTE: Finish comments in style.css first)
+// Planned modules:
+// - EventListener
+// - DOMController
+// - Tasks
+//     - setTask
+//     - getTask
+//     - deleteTask
+// - Projects
+//     - setProject
+//     - getProject
+//     - deleteProject
+
+// 1. Set EventListeners for nav.
+// 2. Set EventListener for Add Task button.
+// 3. Create sample data of Tasks and display in DOM.
+// 4. Create sample data of Projects and display in DOM.
+// 5. Implement Add Task in console only
